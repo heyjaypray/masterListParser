67ccfaeb229d8ed964e2a9089c22e47f95429274
justin <hey@jaypray.com>
2018-01-24 20:27:56 -0700

    update 2

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 // const Scraper = require("email-crawler");
 // const GoogleSearch = require('google-search');
 const fs = require('fs')
+var Scraper = require("email-crawler");
 var recursively = require('recursively');
 const emailScraper = require('./emailScraper.js');
 const googleSearch = require('./googleSearch.js');
 const output = require("./output.json")
+// const data2 = require("./data2.JSON")
 
-let customers = []
-let websites = []
+// let customers = []
+// let emailList = []
+// let websites = [];
+var a;
+
+
+var Database = function(name, email, website) {
+    this.name = name;
+	this.email = email;
+	this.website = website;
+};
+
+//Here in the paranthesis you need to find 
+//a way to pass all 3 parameters for each search...
+
+
+
 
 
 
@@ -17,10 +34,12 @@ let websites = []
     
 
     function search(){
-        for (var i=0; i< 5; i++){
-
+        for (var i=0; i< 10; i++){
+        
+        
         googleSearch.googleSearch.build({
             //Q is the search query built into NPM package
+            
             q: output[i].Customer,
             start: 1,
     
@@ -31,13 +50,27 @@ let websites = []
           }, function(error, response) {
               console.log(response.items[0].link);
 
-              websites.push()
-
+              var websites = response.items[0].link
+              var company = response.items[0].title
+             
+              
               console.log("website " + websites)
+
+
+              var a = new Database(company, "emailList", websites);
+              var makeJSON = function() {
+                  fs.appendFile('data2.json',JSON.stringify(a, null, 2) + ',', function(err) {
+                      err ? console.log(err) : console.log('Content added...');
+                  });
+              }             
+              makeJSON();
+              
         
               
           });
 
+
+          
         };
     };
 
@@ -52,9 +85,10 @@ let websites = []
 
 //Will eventually use this function to scrap emails and add to array
 function scrape(){
-    var emailScraper = new Scraper("http://portadoor.com")
+    var emailScraper = new Scraper(websites)
     emailScraper.getLevels(2).then((emails) => {
         console.log(emails);   
+        emails=emails;
     })
     .catch((e) => {
         console.log("error");
@@ -62,14 +96,13 @@ function scrape(){
 };
 
 
-
-
-
 //Promise so that it wont do google search until array push is done
 var startApp = new Promise (function(resolve, reject){
-    if (customers = output.length) {
-        resolve("asdf")
+    if (true) {
         search();
+
+        console.log("stuff")
+
         
     } else{
         reject("failure")
@@ -79,6 +112,7 @@ var startApp = new Promise (function(resolve, reject){
 //Then Perform Search Function
 startApp.then(function(q){
     console.log('searching')
+
     
 })
 
--- emailScraper.js
+++ emailScraper.js
@@ -1,19 +1,19 @@
-var Scraper = require("email-crawler");
-var emailScraper = new Scraper("http://portadoor.com");
-var emailList = [];
-// A level is how far removed (in  terms of link clicks) a page is from the root page (only follows same domain routes)
+// var Scraper = require("email-crawler");
+// var emailScraper = new Scraper("http://portadoor.com");
+// var emailList = [];
+// // A level is how far removed (in  terms of link clicks) a page is from the root page (only follows same domain routes)
 
-// emailScraper.getLevels(2).then((emails) => {
-//   console.log(emails); // Here are the emails crawled from traveling two levels down this domain
+// // emailScraper.getLevels(2).then((emails) => {
+// //   console.log(emails); // Here are the emails crawled from traveling two levels down this domain
 
-// })
-// .catch((e) => {
-//   console.log("error");
-// })
+// // })
+// // .catch((e) => {
+// //   console.log("error");
+// // })
 
-module.exports = {
-  emailScraper:emailScraper
-}
+// module.exports = {
+//   emailScraper:emailScraper
+// }
 
 
 
