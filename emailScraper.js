2c42ce37d325b01a75cd46b5973fcae571624fcd
Justin <hey@jaypray.com>
2018-01-30 19:13:46 -0700

    asdf

--- emailScraper.js
+++ emailScraper.js
@@ -3,30 +3,32 @@ const output = require("./ctList.json");
 const fs = require('fs');
 
 
+
+
 var counter = 0;
 
-function scrape(name, website, phone, address1, address2, city, state, zip){
-
+const scrape = async (website) => {
   var emailScraper = new Scraper(website);
   console.log(website)
-  counter++;
 
-  emailScraper.getLevels(2).then((emails) => {
-    // console.log(emails); // Here are the emails crawled from traveling two levels down this domain
+  emailScraper
+    .getLevels(1)
+    .then((emails) => {
+      const companyInfo = [website, emails]
+      console.log('Emails', emails)
+      // fs.appendFile('newCustomerListWithEmails.csv', companyInfo.join(',') + '\n', function(err){
+      //   err ? console.log(err) : console.log('Content added...' + counter)
+      //   console.log(emails)
+      // })
 
-    
-    const companyInfo = [name, website, emails, phone, address1, address2, city, state, zip]
-    fs.appendFile('newCustomerListWithEmails.csv', companyInfo.join(',') + '\n', function(err){
-      err ? console.log(err) : console.log('Content added...' + counter)
-      console.log(emails)
+      counter++;
+      console.log(counter)
     })
-  })
-  .catch((e) => {
-    console.log("error");
-  })
 
 
-
+    .catch((e) => {
+      console.log("error");
+    })
 }
 
 
@@ -36,20 +38,18 @@ function scrape(name, website, phone, address1, address2, city, state, zip){
 
 function search(){
 
-
-  for (var i=0; i< 2; i++){
-
-    var name=output[i].Company;
+  for (var i=0; i < 2; i++){
+    // var name=output[i].Company;
     var website=output[i].Website;
-    var phone=output[i].Phone;
-    var address1=output[i].Address1;
-    var address2=output[i].Address2;
-    var city=output[i].City;
-    var state=output[i].State;
-    var zip=output[i].Zip;
+    // var phone=output[i].Phone;
+    // var address1=output[i].Address1;
+    // var address2=output[i].Address2;
+    // var city=output[i].City;
+    // var state=output[i].State;
+    // var zip=output[i].Zip;
 
 
-    scrape(name, website, phone, address1, address2, city, state, zip)
+    scrape(website)
 
   };
 };
